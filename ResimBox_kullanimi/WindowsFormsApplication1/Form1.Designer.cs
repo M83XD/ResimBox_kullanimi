fc008a14e60dc19a5462f0fd2cea3fd17af54bfe
M83 <75308710+M83XD@users.noreply.github.com>
2022-04-21 16:51:06 +0300

    M83

---
+++ ResimBox_kullanimi/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1012, 522);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sonraki";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 522);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Önceki";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 522);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Başla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(931, 522);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Durdur";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 473);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "En Baş";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1011, 473);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "En Son";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApplication1.Properties.Resources.Ekran_Alıntısı;
+            this.pictureBox2.Location = new System.Drawing.Point(551, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(545, 467);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication1.Properties.Resources.Recep_Ivedik_60;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(545, 467);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(490, 475);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1099, 557);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ResimBox_kullanimi/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resimGoster(int deger)
+        {
+            switch (deger)
+            {
+                case 1:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\Recep.jpg");
+                        pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\Recep.jpg");
+                    break;
+                case 2:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\gulenadam.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\gulenadam.jpg");
+                    break;
+                case 3:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\Sage.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\Sage.jpg");
+                    break;
+                case 4:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\tabiefendm.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\tabiefendm.jpg");
+                    break;
+                case 5:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\hiko.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\hiko.jpg");
+                    break;
+                case 6:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\mıho.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\mıho.jpg");
+                    break;
+                case 7:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\At.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\At.jpg");
+                    break;
+                case 8:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\mıho2.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\mıho2.jpg");
+                    break;
+                case 9:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\savunma.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\savunma.jpg");
+                    break;
+                case 10:
+                    pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\kürt1.jpg");
+                    pictureBox2.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\kürt1.jpg");
+                    break;
+            }
+        }
+        int knt = 10;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            knt++;
+            if (knt <= 10)
+                resimGoster(knt);
+            else
+                knt = 10;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            knt--;
+            if (knt <= 10)
+                resimGoster(knt);
+            else
+                knt = 10;
+        }
+        Random rgele = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            resimGoster(rgele.Next(1, 10));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            knt = 1;
+            pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\Recep.jpg");
+            timer1.Enabled = false;
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            knt = 10;
+            pictureBox1.Image = Image.FromFile(@"C:\Users\user\Downloads\Fantezi\kürt1.jpg");
+            timer1.Enabled = false;
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            knt = (int)comboBox1.SelectedItem;
+            resimGoster(knt);
+        }
+    } 
+}
